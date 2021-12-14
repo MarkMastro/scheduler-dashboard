168150ad094033ea705a8f17ba88f497d1bd80f9
MarkMastro <mastropaolo.mark@gmail.com>
2021-12-14 18:49:35 -0500

    create Panel class, add Panel array function to Dashboard class

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
+import Panel from "./Panel";
 
 import classnames from "classnames";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
 
   state = {loading : false}
 
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
+    const panels = data.map( panelData=> <Panel key= {panelData.id} {...panelData} />)
+    
     if(this.state.loading){
 
      return <Loading/>;
